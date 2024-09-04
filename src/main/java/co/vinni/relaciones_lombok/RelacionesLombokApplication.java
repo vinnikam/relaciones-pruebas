a1fcc26594a65c79440fa20bce2a108866ba5f9e
Vinnikam <vinni9@gmail.com>
2024-09-04 08:58:25 -0500

    Proyecto para Relaciones y Pruebas

---
+++ src/main/java/co/vinni/relaciones_lombok/RelacionesLombokApplication.java
@@ -0,0 +1,13 @@
+package co.vinni.relaciones_lombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelacionesLombokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelacionesLombokApplication.class, args);
+    }
+
+}
